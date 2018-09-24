6d16b5efceffeb0c329e74ea714d884d71ea1032
SabariVig <blazingsabari@gmail.com>
2018-09-24 09:18:27 +0000

    Design Tweaks

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(
-                request, f'Account created for {username}!,Now Go Login')
+                request, f'Account created for {username} Now Go Login')
             return redirect('login')
     else:
         form = UserRegisterForm()
